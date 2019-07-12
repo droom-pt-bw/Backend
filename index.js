1990357850889a54cf2d39f3905aa28e92e9611f
Dionuta <diounta@gmail.com>
2019-07-11 19:25:58 -0700

    port

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 
-const {PORT} = require("./config/secrets.js")
 const server = require('./api/server.js');
 
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Running on port ${PORT} **\n`));
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
