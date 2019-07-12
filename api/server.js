6ba8bf5559a5b313bc7e12f0e91bb9187abce33f
Dionuta <diounta@gmail.com>
2019-07-11 22:13:53 -0700

    auth router

--- api/server.js
+++ api/server.js
@@ -5,13 +5,14 @@ const cors = require('cors');
 
 const jobListing = require("../api/router/joblistingRoute");
 const users = require("../api/router/userRoutes");
+const authRoutes = require("../auth/authRoute");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
-
+authRoutes(server);
 server.use("/listings", jobListing);
 server.use("/users", users);
 
